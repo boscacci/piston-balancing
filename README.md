29f0663150781d4feaef79a3e46e9e74c75ca3da
Robert Boscacci <cinemarob1@gmail.com>
2023-04-07 12:00:54 -0400

    Push initial brute force approach

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# piston-balancing
-Match connecting rods, piston pins, and pistons to make optimally uniform weights
+# Piston Balancing
+
+Given n sets of pistons, connecting rods, and pins with different masses:
+
+__How can we assemble [n] sets of (pin, rod, piston) such that the mass variance between sets is minimized?__
---
+++ notebooks/pistons_multithreaded.py
@@ -0,0 +1,80 @@
+import itertools
+import multiprocessing
+import numpy as np
+import time
+
+########### SETTINGS ##########
+
+n_cylinders = 6
+
+### Random data (replace with actual data) ###
+mass_pistons = np.random.uniform(500, 750, n_cylinders).round(1)
+mass_rods = np.random.uniform(400, 600, n_cylinders).round(1)
+mass_pins = np.random.uniform(60, 100, n_cylinders).round(1)
+
+### Actual Data (enter a list of masses): ###
+# mass_pistons = []
+# mass_rods = []
+# mass_pins = []
+
+################################
+
+def timer_decorator(func):
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        end_time = time.time()
+        elapsed_time = end_time - start_time
+        print(f"{func.__name__} took {elapsed_time:.4f} seconds to execute.")
+        return result
+    return wrapper
+
+def weight_variance(sets):
+    total_weights = [sum(s) for s in sets]
+    return np.var(total_weights)
+
+def find_optimal_arrangement(args):
+    rod_combinations, piston_combinations, pin_combinations = args
+    min_var = float('inf')
+    optimal_arrangement = None
+
+    for rods in rod_combinations:
+        for pistons in piston_combinations:
+            for pins in pin_combinations:
+                sets = list(zip(rods, pistons, pins))
+                var = weight_variance(sets)
+                if var < min_var:
+                    min_var = var
+                    optimal_arrangement = sets
+                    print(f"Found smaller var: {round(min_var,4)}")
+                    
+    return min_var, optimal_arrangement
+
+@timer_decorator
+def main():
+    # Calculate all possible combinations of sets
+    rod_combinations = list(itertools.permutations(mass_pins))
+    piston_combinations = list(itertools.permutations(mass_pistons))
+    pin_combinations = list(itertools.permutations(mass_rods))
+
+    # Divide the search space into smaller chunks
+    rod_chunk_size = len(rod_combinations) // (multiprocessing.cpu_count() + 1)
+    rod_chunks = [rod_combinations[i:i + rod_chunk_size] for i in range(0, len(rod_combinations), rod_chunk_size)]
+
+    min_var = float('inf')
+    optimal_arrangement = None
+
+    # Parallelize the computation
+    with multiprocessing.Pool() as pool:
+        results = pool.map(find_optimal_arrangement, [(rod_chunk, piston_combinations, pin_combinations) for rod_chunk in rod_chunks])
+
+        for var, arrangement in results:
+            if var < min_var:
+                min_var = var
+                optimal_arrangement = arrangement
+
+    print("Optimal arrangement:", optimal_arrangement)
+    print("Minimum std:", round(np.sqrt(min_var), 2))
+
+if __name__ == '__main__':
+    main()
